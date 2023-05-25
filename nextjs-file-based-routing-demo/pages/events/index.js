8d1a5070d2700563d2e21d170a102f9b36140d66
Justin van der Kwaak <justinkwaak@hotmail.com>
2023-05-25 09:47:15 +0200

    Finished Section 4 File Based Routing

--- nextjs-file-based-routing-demo/pages/events/index.js
+++ nextjs-file-based-routing-demo/pages/events/index.js
@@ -1,13 +1,24 @@
 import { getAllEvents } from "../../dummy-data";
 import EventList from "../../components/events/EventList";
+import EventSearch from "../../components/events/EventSearch";
+import { Fragment, use } from "react";
+import { useRouter } from "next/router";
 
 function AllEventsPage() {
   const events = getAllEvents();
+  const router = useRouter();
+
+  function findEventsHandler(year, month) {
+    const fullPath = `/events/${year}/${month}`;
+
+    router.push(fullPath);
+  }
 
   return (
-    <div>
+    <Fragment>
+      <EventSearch onSearch={findEventsHandler} />
       <EventList items={events} />
-    </div>
+    </Fragment>
   );
 }
 
